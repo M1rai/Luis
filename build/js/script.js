6c6a1d0e6fcc0b0f1353434fc60baa23da707c90
M1rai <40149245+M1rai@users.noreply.github.com>
2018-12-03 04:39:14 +0300

    Add files via upload

---
+++ build/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+	$('.slider .testimonials-wrap .owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots:true,
+        navText: [],
+        responsive:{
+            0:{
+                items:1,
+                dots:true,
+                nav:false
+            },
+            480:{
+                items:1
+            }
+        }
+    });
+
+    $('.carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        dots:false,
+        navText: [],
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            680:{
+                items:3
+            },
+            1024:{
+                items:4
+            }
+        }
+    });
+
+});
+
